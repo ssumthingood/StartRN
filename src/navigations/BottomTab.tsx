ab886186c64aabe2497dbd7645ceceef2abe9627
ssumthingood <ssumthingood@gmail.com>
2022-07-27 23:09:51 +0900

    0727 bottomtabNavigation

---
+++ src/navigations/BottomTab.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ProfileScreen from '../screens/Profile';
+import HomeScreen from '../screens/Home';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigation = () => {
+  return <></>;
+};
+
+export default BottomTabNavigation;
+
+/* src/navigations/Stack.js */
